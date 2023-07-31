0d875af5a60cab28af206ee1111698d1694bd2bc
Bayma <b.bayma@vertexdigital.co>
2023-07-31 16:36:27 -0300

    configurado env

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const logger = new Logger();
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Nest Start Project')
+    .setDescription('Projeto start Node.Js - NestJs')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    maxAge: 86400,
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3000);
+
+  logger.log(`Server is listening on: ${await app.getUrl()}`);
+}
+bootstrap();
