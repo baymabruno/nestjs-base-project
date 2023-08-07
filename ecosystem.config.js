a4311bebb79ba73fc05ea85b8d7d921f6c4970f8
Bayma <b.bayma@vertexdigital.co>
2023-08-07 11:57:42 -0300

    adicionado gerenciador de aplicação PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     name: 'node-backend',
     script: 'dist/main.js',
-    error_file: '~/.pm2/logs/production.log',
-    out_file: '~/.pm2/logs/production.log',
+    error_file: '/var/www/html/logs/api.log',
+    out_file: '/var/www/html/logs/api.log',
   },
 ];
